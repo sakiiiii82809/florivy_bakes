3ce5153ccfbbdc7f4680e3e485fe672be18c04e5
gaddizack28@gmail.com <gaddizack28@gmail.com>
2026-09-06 22:15:18 +0800

    functions, code cleaning, menu and main page

---
+++ Frontend/Asset/Js/animation.js
@@ -0,0 +1,15 @@
+const fadeElements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+fadeElements.forEach((element) => {
+    observer.observe(element);
+});
